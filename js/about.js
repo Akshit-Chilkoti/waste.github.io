443a0db58cbcc28118d34963207c7c68d8e1fcda
Akshit Chilkoti <86488967+Akshit-Chilkoti@users.noreply.github.com>
2023-05-31 14:06:27 +0530

    uploded my site
    abit about me left

---
+++ js/about.js
@@ -0,0 +1,23 @@
+function checkPassword() {
+  var password = prompt("Opening...");
+
+  if (password === "You Want to see my Account? Ha") {
+    window.open("https://linktr.ee/akshitchilkoti", "_blank");
+  } else {
+    alert("There was an error, please restart your web browser or your computer.");
+  }
+}
+
+
+function checkSite() {
+  var srno = prompt("Please Enter the Serial Number: 1 For Personal Webpage, 2 For Animator8 Inc. Webpage");
+
+  if (srno == 1) {
+    window.open("https://akshitchilkoti.netlify.app", "_blank");
+  } else if (srno == 2) {
+    window.open("https://animator8.netlify.app")
+  }
+  else {
+    alert("Wrong Serial Number, Please retry.")
+  }
+}
--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,12 @@ document.getElementById("myButton").addEventListener("click", function(event) {
   event.preventDefault(); // Prevents the default link behavior
 
   setTimeout(function() {
-    var newTab = window.open("https://akshitchilkoti.netlify.app", "_blank"); // Replace with your desired link
+    var newTab = window.open("about.html", "_blank"); // Replace with your desired link
     newTab.focus();
-  }, 8000); // 5000 milliseconds = 5 seconds
+  }, 2000); 
 });
 
 document.getElementById("myButton").addEventListener("click", function() {
-  alert("If the link does not opens in 10 seconds, please enable pop ups in your web browser. Also you can see about me HERE ON THE TOP→");
+  alert("If the link does not opens in 5 seconds, please enable pop ups in your web browser or click HERE ON THE TOP→");
 });
 
